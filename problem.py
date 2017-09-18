8852c0a49862f7bd3c59e00098f672b428c2f976
David John Gagne <djgagne@ou.edu>
2017-09-18 12:56:00 -0500

    Changed bs rel precision to 5

--- problem.py
+++ problem.py
@@ -16,7 +16,7 @@ workflow = rw.workflows.GridFeatureExtractorClassifier()
 score_types = [
     rw.score_types.BrierSkillScore(name="BSS", precision=3),
     rw.score_types.BrierScore(name="BS", precision=3),
-    rw.score_types.BrierScoreReliability(name="BS Rel", precision=3),
+    rw.score_types.BrierScoreReliability(name="BS Rel", precision=5),
     rw.score_types.BrierScoreResolution(name="BS Res", precision=3),
     rw.score_types.ROCAUC(name="AUC", precision=3),
 ]
